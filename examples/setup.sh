c32d3443accb86a8cdf905135fa7c651e862fcc7
Makis Tracend <makis.tracend@gmail.com>
2014-11-22 07:56:27 -0800

    + example: setup

---
+++ examples/setup.sh
@@ -0,0 +1,15 @@
+
+# Installing the Cloudvisio CLI
+
+# 1. Install the main executable
+npm install -g cloudvisio-cli
+
+#2. Setup cloudvisio (only needed once)
+cloudvisio setup
+
+# you'll be propted with two entries
+# - email: ...
+# - token: ...
+
+# 3. Find out API commands using the help
+cloudvisio -h
